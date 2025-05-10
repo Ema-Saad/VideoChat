9fc9e584697d96e8cb2240fa9d031386345d360c
Ema-Saad <seman1037@gmail.com>
2025-05-10 17:17:42 +0300

    IDK

--- videoChat/wsgi.py
+++ videoChat/wsgi.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 
 import os
 from whitenoise import WhiteNoise
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'videoChat.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application)
+application = WhiteNoise(application,root='/app/static')
