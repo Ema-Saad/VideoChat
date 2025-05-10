119e81e0523d10d711b3a3da08678e4cbeeeafea
Ema-Saad <seman1037@gmail.com>
2025-05-10 13:57:35 +0300

    allowed hosts edited

--- videoChat/settings.py
+++ videoChat/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-_l3weequ+lwy24ral59($h#x77om$ac5xg(v%u(7_e&5of2ban
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',
+                'localhost',
+                'videochat-production-0bb6.up.railway.app']
 
 
 # Application definition
